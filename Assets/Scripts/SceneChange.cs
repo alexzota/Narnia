74b26152e825cf1dd63fb326df7ad2fefae3a544
alexzota <zota.andreialexandru@gmail.com>
2019-05-09 14:42:53 +0300

    Better limits and position of Peter in ER1

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -16,6 +16,7 @@ public class SceneChange : MonoBehaviour
                         SceneManager.LoadScene("EscapeRoom1", LoadSceneMode.Single);
                         PlayerController player = PlayerController.GetInstance();
                         player.gameObject.transform.localScale = new Vector3(25F, 35F, 1F);
+                        player.gameObject.transform.localPosition = new Vector3(-57F,-24F,1F);
                         player.SetSpeed(20);
                         player.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 4;
                         break;
