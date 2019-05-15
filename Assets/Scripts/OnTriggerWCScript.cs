474f8379b81645c73344861f07b53e5488239d46
Cojocaru Miruna <mirunacojocaru26@yahoo.com>
2019-05-15 20:36:27 +0300

    escaperoom2 script

---
+++ Assets/Scripts/OnTriggerWCScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerWCScript : MonoBehaviour
+{
+    public GameObject text;
+    private bool near;
+    private bool moved;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.SetActive(false);
+        near = false;
+        moved = false;
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay2D()
+    {
+        near = true;
+    }
+
+    void Update()
+    {
+        if (near == true)
+        {
+            text.SetActive(true);
+            if (text.activeInHierarchy == true && Input.GetButtonDown("Use") && moved == false)
+            {
+                gameObject.transform.Translate(-20, 0, 0);
+                moved = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D()
+    {
+        near = false;
+        text.SetActive(false);
+    }
+}
+
---
+++ Assets/Scripts/UsScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsScript : MonoBehaviour
+{
+    public GameObject text;
+    public GameObject ticket;
+    private bool ticketVisible;
+    private bool near;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.SetActive(false);
+        near = false;
+        ticketVisible = false;
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay2D()
+    {
+        near = true;
+    }
+
+    void Update()
+    {
+        if (near == true)
+        {
+            text.SetActive(true);
+            if (text.activeInHierarchy == true && Input.GetButtonDown("Use") && ticketVisible == false)
+            {
+                ticket.GetComponent<SpriteRenderer>().sortingOrder = 10;
+                gameObject.GetComponent<SpriteRenderer>().sortingOrder = 2;
+                ticketVisible = true;
+            }
+            else if (text.activeInHierarchy == true && Input.GetButtonDown("Use") && ticketVisible == true)
+            {
+                ticket.GetComponent<SpriteRenderer>().sortingOrder = -1;
+                ticketVisible = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D()
+    {
+        near = false;
+        ticket.GetComponent<SpriteRenderer>().sortingOrder = -1;
+        ticketVisible = false;
+        text.SetActive(false);
+    }
+}
