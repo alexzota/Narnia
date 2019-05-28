36cb3fbce486e5a8379ef8749ffab75fb7e4e07b
Severin Dragos <severin.dragos@gmail.com>
2019-05-28 20:48:33 +0300

    Added dialogue boxes

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,16 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+/// <summary>
+/// Class <c>CameraFollow</c> makes sure the camera is locked onto the layer at all times.
+/// </summary>
 public class CameraFollow : MonoBehaviour
 {
+    /// <summary>
+    /// A refference to the camera's Unity transform object
+    /// </summary>
     private Transform target;
-    private float xMin, xMax, yMin, yMax;
+    /// <summary>
+    /// The x coordinate of the minimal map point
+    /// </summary>
+    private float xMin;
+    /// <summary>
+    /// The x coordinate of the maximal map point
+    /// </summary>
+    private float xMax;
+    /// <summary>
+    /// The y coordinate of the minimal map point
+    /// </summary>
+    private float yMin;
+    /// <summary>
+    /// The y coordinate of the maximal map point
+    /// </summary>
+    private float yMax;
+    /// <summary>
+    /// A refference to the camera's Unity tilemap object
+    /// </summary>
     [SerializeField]
     private Tilemap tilemap;
 
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// </summary>
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
+        target = Player.GetInstance().transform;
 
         Vector3 minTile = tilemap.CellToWorld(tilemap.cellBounds.min);
         Vector3 maxTile = tilemap.CellToWorld(tilemap.cellBounds.max);
@@ -20,12 +47,19 @@ public class CameraFollow : MonoBehaviour
         SetLimits(minTile, maxTile);
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// LateUpdate is called once per frame.
+    /// </summary>
     private void LateUpdate()
     {
         transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), -10);
     }
 
+    /// <summary>
+    /// Sets the camera's boundaries.
+    /// </summary>
+    /// <param name="minTile">The minimal tile of the map</param>
+    /// <param name="maxTile">The maximal tile of the map</param>
     private void SetLimits(Vector3 minTile, Vector3 maxTile)
     {
         Camera cam = Camera.main;
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/// <summary>
+/// Class <c>DialogueManager</c> helps in managing the tutorial dialog boxes.
+/// </summary>
+public class DialogueManager : MonoBehaviour
+{
+    /// <summary>
+    /// Parameter to display the dialogue for Peter.
+    /// </summary>
+    public TextMeshProUGUI dialogueTextPeter;
+    /// <summary>
+    /// Parameter to hold the dialogue for Aslan.
+    /// </summary>
+    public TextMeshProUGUI dialogueTextAslan;
+    /// <summary>
+    /// Reference to an UI component for Aslan's dialogue box.
+    /// </summary>
+    public GameObject dialogueBoxAslan;
+    /// <summary>
+    /// Refence to an UI component for Peter's dialogue box.
+    /// </summary>
+    public GameObject dialogueBoxPeter;
+    /// <summary>
+    /// Holds the sentences that will be displayed on screen.
+    /// </summary>
+    [TextArea(3, 11)]
+    public string[] sentences;
+    /// <summary>
+    /// An index to keep count of the current sentence displayed on screen.
+    /// </summary>
+    public int currentLine;
+
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// </summary>
+    void Start()
+    {
+        dialogueBoxAslan.SetActive(false);
+        dialogueBoxPeter.SetActive(false);
+    }
+
+    /// <summary>
+    /// Update is called once every frame.
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            currentLine++;
+            if (currentLine == 2 || currentLine == 5 || currentLine == 9)
+            {
+                dialogueBoxAslan.SetActive(false);
+                dialogueBoxPeter.SetActive(true);
+            }
+            else
+            {
+                dialogueBoxPeter.SetActive(false);
+                dialogueBoxAslan.SetActive(true);
+            }
+        }
+        if(currentLine>=sentences.Length)
+        {
+
+            dialogueBoxAslan.SetActive(false);
+            dialogueBoxPeter.SetActive(false);
+            
+        }
+        dialogueTextAslan.text = sentences[currentLine];
+        dialogueTextPeter.text = sentences[currentLine];
+    }
+}
